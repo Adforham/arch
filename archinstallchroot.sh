6e31a641eb146b3918436ad0599103b4f5950cf2
Ham <118483760+Adforham@users.noreply.github.com>
2024-08-28 12:42:07 +0300

    i love myself

--- archinstallchroot.sh
+++ archinstallchroot.sh
@@ -6,4 +6,14 @@ locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "waifu" >> /etc/hostname
 mkinitcpio -P
+bootctl install
+echo "default arch.conf" >> /boot/loader/loader.conf
+echo "timeout 3" >> /boot/loader/loader.conf
+echo "console-mode max" >> /boot/loader/loader.conf
+echo "editor no" >> /boot/loader/loader.conf
+echo "title my love" >> /boot/loader/entries/arch.conf
+echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
+echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
+echo "options root=/dev/sda2 rw" >> /boot/loader/entries/arch.conf
 fastfetch
+echo "i'm done nigga"
