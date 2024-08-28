d01081acdf4f9eda4ca0d7f83125651b67e8dcd3
Ham <118483760+Adforham@users.noreply.github.com>
2024-08-29 01:57:05 +0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -16,6 +16,6 @@ echo "title my love" >> /boot/loader/entries/arch.conf
 echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
 echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
 echo "options root=/dev/sda2 rw" >> /boot/loader/entries/arch.conf
-pacman -S --noconfirm base-devel git
+pacman -S --noconfirm base-devel git fastfetch
 fastfetch
 echo "i'm done nigga"
