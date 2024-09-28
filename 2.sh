c85311a571e10e9535e0311d33002aba14974336
Ham <118483760+Adforham@users.noreply.github.com>
2024-09-28 19:42:31 +0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -16,7 +16,7 @@ echo "title arch" >> /boot/loader/entries/arch.conf
 echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
 echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
 echo "options root=/dev/nvme0n1p2 rw" >> /boot/loader/entries/arch.conf
-pacman -S --noconfirm base-devel git bluez bluez-utils pipewire-audio pipewire-pulse pipewire-alsa fastfetch
+pacman -S --noconfirm base-devel git bluez bluez-utils pipewire-audio pipewire-pulse pipewire-alsa fastfetch --needed
 systemctl enable NetworkManager
 systemctl enable bluetooth
 useradd -m ham
