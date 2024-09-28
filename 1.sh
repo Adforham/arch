e75df509ed1be5ea3f0b55f5193c4e76c8a70578
Ham <118483760+Adforham@users.noreply.github.com>
2024-09-28 19:33:27 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-mkfs.btrfs /dev/sda2
-mkfs.fat -F 32 /dev/sda1
-mount /dev/sda2 /mnt
-mount --mkdir /dev/sda1 /mnt/boot
+mkfs.btrfs /dev/nvme0n1p2
+mkfs.fat -F 32 /dev/nvme0n1p1
+mount /dev/nvme0n1p2 /mnt
+mount --mkdir /dev/nvme0n1p1 /mnt/boot
 reflector
 pacstrap -K /mnt base linux linux-firmware intel-ucode sof-firmware networkmanager nano
 genfstab -U /mnt >> /mnt/etc/fstab
