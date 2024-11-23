9d40df4103e09a16a2dfb00eded6f56e07a2943d
Ham <118483760+Adforham@users.noreply.github.com>
2024-11-23 21:25:56 +0200

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-ln -sf /usr/share/zoneinfo/Africa/Cairo /etc/localtime
-hwclock --systohc
+#ln -sf /usr/share/zoneinfo/Africa/Cairo /etc/localtime
+#hwclock --systohc
 rm /etc/locale.gen
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
@@ -15,8 +15,8 @@ echo "editor no" >> /boot/loader/loader.conf
 echo "title arch" >> /boot/loader/entries/arch.conf
 echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
 echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
-echo "options root=/dev/nvme0n1p2 rw" >> /boot/loader/entries/arch.conf
-pacman -S --noconfirm base-devel git bluez pipewire-audio pipewire-pulse pipewire-alsa fastfetch --needed
+echo "options root=/dev/sda2 rw" >> /boot/loader/entries/arch.conf
+#pacman -S --noconfirm base-devel git bluez pipewire-audio pipewire-pulse pipewire-alsa fastfetch --needed
 systemctl enable NetworkManager
 systemctl enable bluetooth
 useradd -m ham
