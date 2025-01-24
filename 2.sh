5b82f0b1180ed72000d95a006227f5ef5678abe8
Ham <118483760+Adforham@users.noreply.github.com>
2025-01-24 15:07:08 +0200

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -15,7 +15,7 @@ echo "editor no" >> /boot/loader/loader.conf
 echo "title arch" >> /boot/loader/entries/arch.conf
 echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
 echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
-echo "options root=/dev/sda2 rw" >> /boot/loader/entries/arch.conf
+echo "options root=/dev/nvme0n1p2 rw" >> /boot/loader/entries/arch.conf
 pacman -S --noconfirm base-devel git pipewire-audio pipewire-pulse pipewire-alsa fastfetch --needed
 systemctl enable NetworkManager
 useradd -m ham
