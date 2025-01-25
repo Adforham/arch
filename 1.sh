5abc752351487df6ced34de0bd30875335cf12f8
Ham <118483760+Adforham@users.noreply.github.com>
2025-01-25 19:43:58 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -4,5 +4,5 @@ mkfs.fat -F 32 /dev/nvme0n1p1
 mount /dev/nvme0n1p2 /mnt
 mount --mkdir /dev/nvme0n1p1 /mnt/boot
 reflector
-pacstrap -K /mnt base linux linux-firmware intel-ucode sof-firmware networkmanager nano
+pacstrap -K /mnt base linux linux-firmware networkmanager nano
 genfstab -U /mnt >> /mnt/etc/fstab
